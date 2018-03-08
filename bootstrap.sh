da6d5ca1edf4de84989a98b867c4a26fc05df845
yvan karmouta <yvan.karmouta@gmail.com>
2018-03-08 18:54:17 +0100

    management interface
    add management interface to userdata

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,22 +3,24 @@
 function boot(){(
     NAME=$1
     IP=$2
+    echo $IP
     openstack server create \
         --key-name deploy \
         --nic net-id=Ext-Net \
         --nic net-id=public \
+	--nic net-id=management \
         --image 'Ubuntu 16.04' \
         --flavor c2-7 \
         --user-data userdata/${NAME/-[0-9]*/} \
         $NAME
 )}
 
-boot deployer
-boot rabbit
-boot mysql
-boot keystone
-boot nova
-boot glance
-boot neutron
-boot horizon
-boot compute-1
+boot deployer 192.168.0.10
+#boot rabbit
+#boot mysql
+#boot keystone
+#boot nova
+#boot glance
+#boot neutron
+#boot horizon
+#boot compute-1
